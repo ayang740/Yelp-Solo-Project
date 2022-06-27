a9b9e2eb3e5063c7c468101a4edde1be9a3dc9c1
allan <ayang740@gmail.com>
2022-06-27 10:35:57 -0400

    reviews migration

--- backend/db/migrations/20220627143101-create-review.js
+++ backend/db/migrations/20220627143101-create-review.js
@@ -1,33 +1,30 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Pizzerias', {
+    return queryInterface.createTable('Reviews', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      name: {
+      review: {
         allowNull: false,
-        type: Sequelize.STRING(50)
+        type: Sequelize.TEXT
       },
-      openingTime: {
+      rating: {
         allowNull: false,
         type: Sequelize.INTEGER
-      },
-      closingTime: {
-        allowNull: false,
-        type: Sequelize.INTEGER
-      },
-      address: {
-        allowNull: false,
-        type: Sequelize.STRING(100)
       },
       userId: {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: { model: "Users" },
+      },
+      pizzeriaId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: "Pizzerias"}
       },
       createdAt: {
         allowNull: false,
@@ -36,10 +33,10 @@ module.exports = {
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
-      },
+      }
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Pizzerias');
+    return queryInterface.dropTable('Reviews');
   }
 };
