926aea8e8cdf03f43fc80d9ac0e719d95944d843
allan <ayang740@gmail.com>
2022-06-27 16:23:35 -0400

    review seeders

--- backend/db/seeders/20220627194318-reviewSeeds.js
+++ backend/db/seeders/20220627194318-reviewSeeds.js
@@ -7,11 +7,32 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+      */
+      return queryInterface.bulkInsert('Reviews', [{
+        review: 'Hellboy slice was incredible! It had a crispy crust and delicious pepperonis. The drizzle of hot honey on top sealed the deal as the best slice I\'ve ever had.',
+        rating: 5,
+        userId: 1,
+        pizzeriaId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        review: 'A solid slice of cheese pizza. Although it was good I cant\'t say they live up to their name of best pizza.',
+        rating: 4,
+        userId: 2,
+        pizzeriaId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        review: 'The best crust to sauce to cheese ratio I\'ve ever had.',
+        rating: 5,
+        userId: 3,
+        pizzeriaId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
@@ -20,7 +41,7 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+      */
+      return queryInterface.bulkDelete('Reviews', null, {});
   }
 };
