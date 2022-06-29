db73fab5efccadf6e6258f5aaa7e2e813eca5ae7
allan <ayang740@gmail.com>
2022-06-29 10:57:12 -0400

    adding new pizzeria route

--- backend/routes/api/pizzerias.js
+++ backend/routes/api/pizzerias.js
@@ -6,6 +6,7 @@ const { PizzaPlace } = require("../../db/models");
 
 const router = express.Router();
 
+//get all pizza places
 router.get(
     "/",
     asyncHandler(async (req, res) => {
@@ -13,6 +14,27 @@ router.get(
       return res.json(pizzerias);
     })
   );
+
+//post new pizza place
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { name, openingTime, closingTime, address, userId } = req.body;
+    const pizzeria = await PizzaPlace.create({
+      name,
+      openingTime,
+      closingTime,
+      address,
+      userId
+    });
+
+    return res.json({
+      pizzeria,
+    });
+  })
+);
+
+
 
 
 module.exports = router;
